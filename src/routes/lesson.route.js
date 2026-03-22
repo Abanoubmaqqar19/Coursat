6a8c4c54c16c74375245e1be03b70cdd20bc8ae2
Abanoub Maqqar <abanoubmaqqar19@gmail.com>
2026-03-22 22:07:25 +0200

    feat: add Lesson controller and routes

--- src/routes/lesson.route.js
+++ src/routes/lesson.route.js
@@ -1,4 +1,17 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
 //*controller methods
+const {
+  getLessons,
+  getLesson,
+  createLesson,
+  updateLesson,
+  deleteLesson,
+} = require("../controllers/lesson.controler");
+
+router.get("/", getLessons);
+router.get("/:id", getLesson);
+router.post("/", createLesson);
+router.put("/:id", updateLesson);
+router.delete("/:id", deleteLesson);
 
 module.exports = router;
